800cf70c17a275ad8092bb79231b0054ce3f1572
bdawg198 <brodyjensenyt@gmail.com>
2026-08-08 14:44:51 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pc-flip-ledger-v16";
+const CACHE_NAME = "pc-flip-ledger-v18";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(["./", "./index.html", "./manifest.webmanifest"])));
